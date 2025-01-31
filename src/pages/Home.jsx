e83a16a7cfce86861b11c12a3abc1d92ad0d8621
ShreeshantGmr764 <withgamergaming764@gmail.com>
2025-01-31 17:29:40 +0545

    Analytics Added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import ContactComponent from '../components/contactComponent'
+import { Analytics } from "@vercel/analytics/react"
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Navbar/>
       <Hero/>
       <ContactComponent/>
+      <Analytics/>
     </div>
   )
 }
